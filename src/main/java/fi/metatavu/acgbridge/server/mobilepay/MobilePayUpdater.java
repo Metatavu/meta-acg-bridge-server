c84ef191b10c56fe296c1f57afdb9dbb6768f9a8
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-22 14:18:44 +0300

    Changed warmup time for the scheduler

--- src/main/java/fi/metatavu/acgbridge/server/mobilepay/MobilePayUpdater.java
+++ src/main/java/fi/metatavu/acgbridge/server/mobilepay/MobilePayUpdater.java
@@ -26,7 +26,7 @@ public class MobilePayUpdater {
   
   @PostConstruct
   public void postConstruct() {
-    startTimer(1200, 1200);
+    startTimer(60000, 1200);
   }
   
   private void startTimer(long warmup, long delay) {
