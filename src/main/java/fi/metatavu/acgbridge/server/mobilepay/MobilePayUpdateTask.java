492803b1a38861e35c74a3684f78c3c490331841
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-28 13:31:32 +0300

    Fixed updater transactio

--- src/main/java/fi/metatavu/acgbridge/server/mobilepay/MobilePayUpdateTask.java
+++ src/main/java/fi/metatavu/acgbridge/server/mobilepay/MobilePayUpdateTask.java
@@ -10,8 +10,8 @@ import javax.annotation.Resource;
 import javax.ejb.EJBContext;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -58,17 +58,29 @@ public class MobilePayUpdateTask implements Runnable {
   
   @Resource
   private EJBContext ejbContext;
-  
+
   @Override
+  @SuppressWarnings ("squid:S2583")
   public void run() {
-    runTx();
+    UserTransaction userTransaction = ejbContext.getUserTransaction();
+    try {
+      userTransaction.begin();
+      
+      checkPendingTransactions();
+      
+      userTransaction.commit();
+    } catch (Exception ex) {
+      logger.log(Level.SEVERE, "Timer throw an exception", ex);
+      try {
+        if (userTransaction != null) {
+          userTransaction.rollback();
+        }
+      } catch (SystemException e1) {
+        logger.log(Level.SEVERE, "Failed to rollback transaction", e1);
+      }
+    }
   }
 
-  @Transactional (value = TxType.REQUIRES_NEW)
-  public void runTx() {
-    checkPendingTransactions();
-  }
-  
   private void checkPendingTransactions() {
     List<MobilePayTransaction> transactions = transactionController.listPendingMobilePayTransactions(clusterController.getLocalNodeName());
     for (MobilePayTransaction transaction : transactions) {
@@ -98,7 +110,7 @@ public class MobilePayUpdateTask implements Runnable {
     if (response.isOk()) {
       PaymentStatusResponse paymentStatus = response.getResponse();
       
-      logger.log(Level.INFO, () -> String.format("MobilePay responded with %d for payment %d", paymentStatus.getPaymentStatus(), transaction.getId()));
+      logger.log(Level.SEVERE, () -> String.format("MobilePay responded with %d for payment %d", paymentStatus.getPaymentStatus(), transaction.getId()));
       
       switch (paymentStatus.getPaymentStatus()) {
         case 40: // Cancel
@@ -133,7 +145,7 @@ public class MobilePayUpdateTask implements Runnable {
       ReservationStatusResponse reservationStatusResponse = mobilePayResponse.getResponse();
       Integer reservationStatus = reservationStatusResponse.getReservationStatus();
       
-      logger.log(Level.INFO, () -> String.format("MobilePay responded with %d for reservation %d", reservationStatus, transaction.getId()));
+      logger.log(Level.SEVERE, () -> String.format("MobilePay responded with %d for reservation %d", reservationStatus, transaction.getId()));
       
       switch (reservationStatus) {
         case 40: // Cancel
